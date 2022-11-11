a3a0454457608659825ecfd2cfd7e4cb9639382d
happycode202 <sunhongleisun@gmail.com>
2022-11-12 02:34:22 +0800

    init

---
+++ huya.bat
@@ -0,0 +1,15 @@
+echo "常看虎牙直播地址解析"
+python C:\Users\sunho\Documents\zhibo\huya/小老虎.py
+echo "======================"
+python C:\Users\sunho\Documents\zhibo\huya/奥斯丁.py
+echo "======================"
+python C:\Users\sunho\Documents\zhibo\huya/杨凯pk4.py
+echo "======================"
+python C:\Users\sunho\Documents\zhibo\huya/小飞.py
+echo "======================"
+python C:\Users\sunho\Documents\zhibo\huya/弑神.py
+echo "======================"
+python C:\Users\sunho\Documents\zhibo\huya/荒年.py
+echo "======================"
+python C:\Users\sunho\Documents\zhibo\huya/大雄小清新.py
+
---
+++ zhibo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "START"
+echo "====================="
+python3 /Users/sunhl/Documents/zhibo/huya/荒年.py
+echo "====================="
+python3 /Users/sunhl/Documents/zhibo/huya/小飞.py
+echo "====================="
+python3 /Users/sunhl/Documents/zhibo/huya/奥斯丁.py
+echo "====================="
+python3 /Users/sunhl/Documents/zhibo/huya/大雄小清新.py
+echo "====================="
+python3 /Users/sunhl/Documents/zhibo/huya/弑神.py
+echo "====================="
+python3 /Users/sunhl/Documents/zhibo/huya/杨凯pk4.py
+echo "====================="
+python3 /Users/sunhl/Documents/zhibo/huya/小老虎.py
+
+echo "====================="
+echo "END"
+
